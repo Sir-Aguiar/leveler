365a5f2a5233c9e77e84e478db85856c728bfc9b
Felipe Aguiar <felipeferreiraaguiar9@gmail.com>
2025-04-20 18:23:57 -0400

    .

--- src/main/java/org/aguiar/leveler/events/RaidZombieDeathListener.java
+++ src/main/java/org/aguiar/leveler/events/RaidZombieDeathListener.java
@@ -1,14 +1,28 @@
 package org.aguiar.leveler.events;
 
+import org.aguiar.leveler.Leveler;
+import org.aguiar.leveler.utils.LevelerPlayerData;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.metadata.MetadataValue;
+import org.bukkit.plugin.Plugin;
+
+import java.util.List;
 
 public class RaidZombieDeathListener implements Listener {
+
+  private final Leveler plugin;
+
+  public RaidZombieDeathListener(Leveler plugin) {
+    this.plugin = plugin;
+  }
+
   @EventHandler
   public void onDeath(EntityDeathEvent event) {
     Zombie entity = (Zombie) event.getEntity();
@@ -30,4 +44,9 @@ public class RaidZombieDeathListener implements Listener {
     player.sendMessage(String.format("Você ganhou %.2f de experiência!", baseExperience));
   }
 
+
+  @EventHandler
+  public void onJoin(PlayerJoinEvent event) {
+    Bukkit.getConsoleSender().sendMessage("Jogador entrou com sucesso");
+  }
 }
