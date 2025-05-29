5c1f89b228a06d4c2c75249ee440ac84debac43e
Felipe Aguiar <felipeferreiraaguiar9@gmail.com>
2025-05-29 16:57:35 -0400

    Making my life easier to set barriers in dungeon levels

--- src/main/java/org/aguiar/leveler/entities/raids/Raid.java
+++ src/main/java/org/aguiar/leveler/entities/raids/Raid.java
@@ -1,16 +1,31 @@
 package org.aguiar.leveler.entities.raids;
 
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Monster;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Raid {
-  private final World world;
-  private final List<RaidMob> mobs = new ArrayList<>();
+  private final List<Entity> mobs = new ArrayList<>();
 
-  public Raid(World world) {
-    this.world = world;
+  public Monster spawnMob(Location location, RaidMob raidMob) {
+    World world = location.getWorld();
+    assert world != null;
+
+    Monster spawnedEntity = (Monster) world.spawnEntity(location, raidMob.entityType());
+
+    ItemStack testItem = new ItemStack(Material.CHAINMAIL_HELMET);
+
+    spawnedEntity.getEquipment().getItem().add
+
+    return spawnedEntity;
   }
 
   public void startRaid() {
--- src/main/java/org/aguiar/leveler/entities/raids/RaidMob.java
+++ src/main/java/org/aguiar/leveler/entities/raids/RaidMob.java
@@ -1,5 +1,25 @@
 package org.aguiar.leveler.entities.raids;
 
-public abstract class RaidMob {
+import org.bukkit.attribute.Attribute;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.EntityType;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.List;
+import java.util.Map;
+
+
+
+public record RaidMob(
+        String name,
+        EntityType entityType,
+        List<Map<Attribute, Double>> attributes,
+        List<Map<EquipmentSlot, ItemStack>> equipments,
+        List<Map<EquipmentSlot, Enchantment>> enchantments,
+        double health,
+        double damage,
+        double xpDrop
+) {
 
 }
